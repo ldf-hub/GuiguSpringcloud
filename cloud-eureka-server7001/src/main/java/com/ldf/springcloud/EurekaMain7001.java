5e832472a47cf91e0c67b0b35b6eab918aa57046
ldf-hub <67501985+ldf-hub@users.noreply.github.com>
2023-01-29 16:25:38 +0800

    单机版eureka构建

--- cloud-eureka-server7001/src/main/java/com/ldf/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/ldf/springcloud/EurekaMain7001.java
@@ -2,16 +2,16 @@ package com.ldf.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author ldf
- * @create 2023/1/12 15:27
+ * @create 2023年1月11日14:05:58
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/ldf/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/ldf/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.ldf.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ldf
  * @create 2023年1月11日14:05:58
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
