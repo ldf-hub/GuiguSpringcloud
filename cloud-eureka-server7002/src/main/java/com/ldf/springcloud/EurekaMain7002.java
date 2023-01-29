784b58c719bcb08c44e76580caad56bb34334cae
ldf-hub <67501985+ldf-hub@users.noreply.github.com>
2023-01-29 17:08:35 +0800

    eureka集群搭建

---
+++ cloud-eureka-server7002/src/main/java/com/ldf/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.ldf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author ldf
+ * @create 2023/1/29 16:52
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
