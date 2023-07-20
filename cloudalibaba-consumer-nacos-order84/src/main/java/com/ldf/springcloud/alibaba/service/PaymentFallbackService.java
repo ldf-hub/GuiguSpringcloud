584bb9cc79ee54a4f002002d160d2ebedb3d9ee6
ldf-hub <67501985+ldf-hub@users.noreply.github.com>
2023-07-20 15:48:57 +0800

    sentinel使用

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/ldf/springcloud/alibaba/service/PaymentFallbackService.java
@@ -0,0 +1,17 @@
+package com.ldf.springcloud.alibaba.service;
+
+import entities.CommonResult;
+import entities.Payment;
+import org.springframework.stereotype.Component;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-25 18:30
+ */
+@Component
+public class PaymentFallbackService implements PaymentService {
+    @Override
+    public CommonResult<Payment> paymentSQL(Long id) {
+        return new CommonResult<>(44444, "服务降级返回,---PaymentFallbackService", new Payment(id, "errorSerial"));
+    }
+}
